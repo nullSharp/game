4c5ee84d5d161818ea516c0717f9f9011c322703
Vakushev Dmitriy <42519548+nullSharp@users.noreply.github.com>
2018-09-08 06:21:47 +0300

    rockets

--- Documents/SKCOOL or much of pain/Assets/bomb.cs
+++ Documents/SKCOOL or much of pain/Assets/bomb.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class bomb : MonoBehaviour {
-
+    private bool c = true;
 	// Use this for initialization
 	void Start () {
-        for (int i = 0; i < 5; i++)
-        {
-            transform.localScale *= 1.1f;
-        }
-        Destroy(gameObject);
+       
     }
 	
 	// Update is called once per frame
 	void Update () {
-       
+       if(c)
+        {
+            StartCoroutine(TI());
         }
-    
+        }
+    IEnumerator TI()
+    {
+        c = false;
+        yield return new WaitForSeconds(0.3f);    
+        transform.localScale *= 1.1f;
+        yield return new WaitForSeconds(0.3f);
+        transform.localScale *= 1.1f;
+        yield return new WaitForSeconds(0.3f);
+        transform.localScale *= 1.1f;
+        Destroy(gameObject);
+    }
 }
